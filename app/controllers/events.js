57938ed386fe3497ebce45eca67c8311e87284e0
David Tang <david.tang@verizondigitalmedia.com>
2019-02-09 13:04:23 -0800

    make circle buttons smaller

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,7 +1,8 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
-const PAGE_ID = '1118053411550221';
+// const PAGE_ID = '1118053411550221'; // av
+const PAGE_ID = '107774535942892'; // I wanna rage
 // https://developers.facebook.com/docs/graph-api/reference/page/events/
 
 export default Controller.extend({
@@ -9,13 +10,13 @@ export default Controller.extend({
   actions: {
     login() {
       this.torii.open('facebook-connect').then(() => {
-        FB.api(`/${PAGE_ID}`, {
-          time_filter: 'upcoming'
-        }, function(response) {
-          console.log(response);
-        });
+        // FB.api(`/${PAGE_ID}/events`, {
+        //   time_filter: 'upcoming'
+        // }, function(response) {
+        //   console.log(response);
+        // });
 
-        FB.api('/113124472034820', function(response) {
+        FB.api(`/${PAGE_ID}`, function(response) {
           console.log(response);
         });
       });
