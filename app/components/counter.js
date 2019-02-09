6ac918ccf2a0f67fd45d36af0a3f73d0268fb6be
David Tang <david.tang@verizondigitalmedia.com>
2019-02-09 14:06:11 -0800

    use session storage instead of local storage

--- app/components/counter.js
+++ app/components/counter.js
@@ -2,17 +2,17 @@ import Component from '@ember/component';
 import { equal } from '@ember/object/computed';
 
 export default Component.extend({
-  totalCount: localStorage.totalCount || 0,
+  totalCount: sessionStorage.totalCount || 0,
   cantDecrement: equal('totalCount', 0),
   actions: {
     increment() {
       this.incrementProperty('totalCount');
-      localStorage.totalCount = this.totalCount;
+      sessionStorage.totalCount = this.totalCount;
     },
     decrement() {
       if (this.totalCount > 0) {
         this.decrementProperty('totalCount');
-        localStorage.totalCount = this.totalCount;
+        sessionStorage.totalCount = this.totalCount;
       }
     }
   }
